bfbdde14bc14f7234fa8c3164df24790c4e2b8ae
SOUMYA\ersau <ersaumyarout@gmail.com>
2024-04-11 12:14:30 +0530

    material table added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MasterService } from './services/master.service';
+import { Product } from './models/product';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Crud17App';
+export class AppComponent implements OnInit {
+
+  productList!:Product[];
+  dataSources:any;
+  displayedColumns:string[]=['id','name','description','price'];
+
+  constructor(private service:MasterService){}
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct(){
+    this.service.getAll().subscribe(item=>{
+      this.productList=item;
+      this.dataSources=new MatTableDataSource(this.productList);
+    })
+    
+  }
 }
